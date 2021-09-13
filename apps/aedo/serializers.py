478435773fa8cb3a4b51a15b267f4a88f20171d8
mdominguez <mocomauricio@gmail.com>
2021-09-13 14:46:11 -0400

    fix bug not null fechas

--- apps/aedo/serializers.py
+++ apps/aedo/serializers.py
@@ -22,8 +22,8 @@ class DeliverySerializer(serializers.ModelSerializer):
         representation = super().to_representation(instance)
         representation['company'] = instance.company.name
         representation['employee'] = '<div><img class="img-fluid" src="%s"  width="100" height="100"></div><p>%s</p>' % (instance.employee.photo.url, instance.employee.get_full_name()) if (instance.employee) != None else "A definir"
-        representation['reception_date'] = '%s %s' % (instance.reception_date.strftime("%d/%m/%Y"), instance.reception_time.strftime("%H:%M")), 
-        representation['deliver_date'] = '%s %s' % (instance.deliver_date.strftime("%d/%m/%Y"), instance.deliver_time.strftime("%H:%M")), 
+        representation['reception_date'] = '%s %s' % (instance.reception_date.strftime("%d/%m/%Y"), instance.reception_time.strftime("%H:%M")) if (instance.reception_date != None and instance.reception_time != None) else ""  
+        representation['deliver_date'] = '%s %s' % (instance.deliver_date.strftime("%d/%m/%Y"), instance.deliver_time.strftime("%H:%M")) if (instance.deliver_date != None and instance.deliver_time != None) else "" 
         representation['total'] = instance.get_total()
         representation['state'] = instance.get_state_display()
         representation['pending'] = instance.get_pending()
